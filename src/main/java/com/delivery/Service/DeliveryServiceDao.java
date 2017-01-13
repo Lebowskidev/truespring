263850c89f4508191e14939785149c85a32880ec
Lebowskidev <andriy.auditore@gmail.com>
2017-01-13 10:36:36 +0200

    complete issuse 1

--- src/main/java/com/delivery/Service/DeliveryServiceDao.java
+++ src/main/java/com/delivery/Service/DeliveryServiceDao.java
@@ -28,6 +28,58 @@ public class DeliveryServiceDao implements DeliveryInterface  {
 		deli.setPrice(rs.getInt(3));
 
 		return deli;
+		}
 	}
-}
+		/**
+		 * surch elements
+		 * @param id
+		 * @return deliveryEntity object deli
+		 */
+	public deliveryEntity surchById(int id){
+		final String SQL = "SELECT * FROM menu WHERE id=?";
+		deliveryEntity deli = jdbc.queryForObject(SQL, new workingWithRowMap());
+		return deli;
+	}
+	
+		/**
+		 * update data
+		 * @param deliveryEntity object deli
+		 */
+	public void updateDelivery(deliveryEntity deli){
+		final String SQL = "UPDATE menu SET goods=?, price=? WHERE id=?";
+		final int id = deli.getId();
+		final String goods = deli.getGoods();
+		final int price = deli.getPrice();
+
+jdbc.update(SQL, new Object[] { goods, price, id });
+	}
+	/**
+	 * view elements 
+	 * @return deliveryEntity object deli
+	 */
+	public Collection<deliveryEntity> infoDelivery(int page) {
+		page *= 3;
+		final String SQL = "SELECT * FROM menu LIMIT 3 offset ?";
+		List<deliveryEntity> deli = jdbc.query(SQL, new workingWithRowMap(), page);
+		return deli;
+	}
+	/**
+	 * delete data by id
+	 */
+    public void removeDelivery(int id){
+    	final String SQL = "DELETE FROM menu WHERE id=?";
+    	jdbc.update(SQL,id);
+    }
+    /**
+     * create new data    
+     */
+    public void createDekivery(deliveryEntity deli){
+    	
+    	final String SQL = "INSERT INTO menu (goods, price) values (?, ?) ";
+		final String goods = deli.getGoods();
+		final int price = deli.getPrice();
+
+		jdbc.update(SQL, new Object[] { goods, price });
+    }
+    
 }
